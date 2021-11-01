5473f4051af94c287e1e9528807a121924039d22
MathieuFourmont <91331763+MathieuFourmont@users.noreply.github.com>
2021-11-01 13:11:29 +0000

    Add files via upload

--- 00_Libft/libft.h
+++ 00_Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mmaxime- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 15:51:09 by mmaxime-          #+#    #+#             */
-/*   Updated: 2021/10/29 11:13:55 by mmaxime-         ###   ########.fr       */
+/*   Updated: 2021/11/01 13:09:34 by mmaxime-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@
 ** ------------------------------ LIBRARIES ------------------------------
 */
 
-#include <unistd.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <stdlib.h>
 
 /*
 ** ------------------------------ STRUCT DECLARATION ----------------------
 */
 
-typedef struct	s_list
+typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
@@ -47,8 +47,8 @@ void	ft_lstadd_back(t_list **alst, t_list *new);
 void	ft_lstadd_front(t_list **alst, t_list *new);
 void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
-t_list 	*ft_lstlast(t_list *lst);
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *));
+t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 t_list	*ft_lstnew(void *content);
 int		ft_lstsize(t_list *lst);
 void	ft_lstiter(t_list *lst, void (*f)(void *));
