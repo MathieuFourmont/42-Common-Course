fa6e671335c60801ff8a22a9f603c87edafd6a13
MathieuFourmont <91331763+MathieuFourmont@users.noreply.github.com>
2022-03-28 16:16:09 +0100

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmaxime- <mmaxime-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/03 14:24:22 by mmaxime-          #+#    #+#             */
+/*   Updated: 2022/03/28 12:08:19 by mmaxime-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_get_line(int fd, char **save, char *buffer);
+char	*ft_return_line(char **save, char *temp, int new_line_index);
+void	ft_free(char **ptr);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strchr(const char *s, int c);
+
+# define OPEN_MAX 256
+
+#endif
