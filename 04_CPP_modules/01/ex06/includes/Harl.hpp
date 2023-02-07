7d0f61626d796bef5adc383c948c168dcac197da
Mathieu Fourmont <91331763+MathieuFourmont@users.noreply.github.com>
2023-02-07 12:31:34 +0100

    Add files via upload

--- 04_CPP_modules/01/ex06/includes/Harl.hpp
+++ 04_CPP_modules/01/ex06/includes/Harl.hpp
@@ -10,8 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef HARL_HPP
-# define HARL_HPP
+#ifndef HARL_H
+# define HARL_H
 
 #include <iostream>
 #include <string>
