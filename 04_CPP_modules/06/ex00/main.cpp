c7b55d5545a82b38601fad072773da9f469dc8d5
Mathieu Fourmont <91331763+MathieuFourmont@users.noreply.github.com>
2023-02-07 17:54:28 +0100

    Add files via upload

--- 04_CPP_modules/06/ex00/main.cpp
+++ 04_CPP_modules/06/ex00/main.cpp
@@ -24,5 +24,29 @@ int	main( int argc, char **argv )
 		return ( -1 );
 	}
 
-	double	d = std::atof()
+	double	d = std::atof( argv[1] );
+	int		i = static_cast<int>( d );
+	float	f = static_cast<float>( d );
+	char	c = static_cast<char>( d );
+
+	std::cout << std::fixed << std::setprecision( 1 ); // pour mettre 1 chiffre apres la virgule lorsque le type le permet
+
+	// CHAR
+	if ( d > CHAR_MAX || d < CHAR_MIN )
+		std::cout << "char: impossible" << std::endl;
+	std::cout << "char: " << c << std::endl;
+
+
+	// INT
+	std::cout << "int: " << i << std::endl;
+
+
+	// FLOAT
+	std::cout << "float: " << f << "f" << std::endl;
+
+
+	// DOUBLE
+	std::cout << "double: " << d << std::endl;
+
+	return ( 0 );
 }
