8a07ddc057cce3fa464c204004fc6137a8766d12
Mathieu Fourmont <91331763+MathieuFourmont@users.noreply.github.com>
2023-02-18 14:35:03 +0100

    Add files via upload

---
+++ cpp_08_a_checker/ex01/includes/Span.hpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmaxime- <mmaxime-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/14 14:53:14 by mmaxime-          #+#    #+#             */
+/*   Updated: 2023/02/16 12:43:13 by mmaxime-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SPAN_H
+# define SPAN_H
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <exception>
+#include <limits>
+#include <ctime>
+#include <cstdlib>
+
+class Span
+{
+	public:
+		Span();
+		Span( unsigned int n );
+		Span( Span const & src );
+		~Span();
+
+		Span &				operator=( Span const & rhs );
+
+		void				addNumber( int nb );
+		unsigned int		shortestSpan() const;
+		unsigned int		longestSpan() const;
+		void				fillRandom();
+		void				printSpan();
+
+	private:
+		std::vector<int>	_span;
+};
+
+#endif
