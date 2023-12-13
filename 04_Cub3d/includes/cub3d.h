eacfa52c74ec2e827483aa7535af329c4abfacb9
Mathieu Fourmont <91331763+MathieuFourmont@users.noreply.github.com>
2023-12-13 19:40:17 +0100

    Final -> 110%

---
+++ 04_Cub3d/includes/cub3d.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmaxime- <mmaxime-@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/04 10:34:18 by plavergn          #+#    #+#             */
+/*   Updated: 2023/12/12 17:24:42 by mmaxime-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <stdio.h>
+# include "../mlx_Linux/mlx.h"
+# include "get_next_line.h"
+# include <stdlib.h>
+# include <math.h>
+# include <fcntl.h>
+# include "cub3d_define.h"
+# include "cub3d_structures.h"
+# include "cub3d_functions.h"
+
+#endif
